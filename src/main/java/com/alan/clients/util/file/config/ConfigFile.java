f5ae2090987350a78786f7f63b37857820a6faf8
OpenRiseClient <noreply@openriseclient.invalid>
2026-08-15 14:01:44 +0000

    apply loaded configs locally

--- src/main/java/com/alan/clients/util/file/config/ConfigFile.java
+++ src/main/java/com/alan/clients/util/file/config/ConfigFile.java
@@ -15,12 +15,16 @@ import com.alan.clients.value.impl.ListValue;
 import com.alan.clients.value.impl.ModeValue;
 import com.alan.clients.value.impl.NumberValue;
 import com.alan.clients.value.impl.StringValue;
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.alan.clients.ui.theme.Themes;
 import com.alan.clients.util.localization.Locale;
 import com.alan.clients.util.localization.Localization;
+import com.alan.clients.util.vector.Vector2d;
 import com.alan.clients.component.impl.render.NotificationComponent;
 import com.alan.clients.newevent.impl.other.ConfigLoadEvent;
 import com.alan.clients.util.interfaces.Bindable;
+import java.awt.Color;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.FileReader;
@@ -28,6 +32,8 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.HashMap;
+import java.util.Map;
 import lombok.Generated;
 import rip.vantage.commons.packet.impl.client.protection.C2SPacketConfig;
 import rip.vantage.network.core.VantageNetwork;
@@ -64,6 +70,7 @@ public class ConfigFile extends File implements Bindable {
             }
 
             e(jsonobject);
+            apply(jsonobject);
             if (Client.a.getConfigManager().mQ().T(1000L)) {
                 VantageNetwork.aKB().aKK().sendMessage(new C2SPacketConfig(jsonobject.toString()).aJk());
                 Client.a.getConfigManager().mQ().aX();
@@ -190,6 +197,176 @@ public class ConfigFile extends File implements Bindable {
 
         jsonobject.addProperty("theme", Client.a.getThemeManager().getTheme().name());
         return jsonobject;
+    }
+
+    //add code
+    private static void apply(JsonObject json) {
+        Map<String, Module> map = new HashMap<>();
+
+        for (Module module : Client.a.g().getAll()) {
+            if (!(module instanceof ClickGUI)) {
+                module.setEnabled(false);
+            }
+
+            String s = module.getModuleInfo().aliases()[0];
+            map.put(s, module);
+
+            for (Locale locale : Locale.values()) {
+                String s1 = Localization.a(s, locale);
+                if (s1 != null && !s1.isEmpty() && !s1.equals(s)) {
+                    map.putIfAbsent(s1, module);
+                }
+            }
+
+            for (Value<?> value : module.getAllValues()) {
+                try {
+                    value.setValueAsObject(value.getDefaultValue());
+                } catch (Throwable throwable) {
+                }
+            }
+        }
+
+        if (json.has("theme")) {
+            try {
+                Client.a.getThemeManager().a(Themes.valueOf(json.get("theme").getAsString()));
+            } catch (Throwable throwable) {
+            }
+        }
+
+        for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+            Module module = map.get(entry.getKey());
+            if (module == null || !entry.getValue().isJsonObject()) {
+                continue;
+            }
+
+            Map<String, JsonObject> map1 = new HashMap<>();
+
+            for (Map.Entry<String, JsonElement> entry1 : entry.getValue().getAsJsonObject().entrySet()) {
+                if (entry1.getValue().isJsonObject()) {
+                    map1.putIfAbsent(entry1.getKey().toLowerCase(java.util.Locale.ROOT), entry1.getValue().getAsJsonObject());
+                }
+            }
+
+            for (Value<?> value : module.getAllValues()) {
+                try {
+                    JsonObject jsonobject = map1.get(f(value).toLowerCase(java.util.Locale.ROOT));
+                    if (jsonobject != null) {
+                        a(value, jsonobject);
+                    }
+                } catch (Throwable throwable) {
+                }
+            }
+        }
+
+        for (Map.Entry<String, JsonElement> entry : json.entrySet()) {
+            Module module = map.get(entry.getKey());
+            if (module == null || !entry.getValue().isJsonObject()) {
+                continue;
+            }
+
+            JsonObject jsonobject = entry.getValue().getAsJsonObject();
+
+            try {
+                if (jsonobject.has("state") && !(module instanceof ClickGUI)) {
+                    module.setEnabled(jsonobject.get("state").getAsBoolean());
+                }
+            } catch (Throwable throwable) {
+            }
+
+            try {
+                if (jsonobject.has("keyCode")) {
+                    module.setKey(jsonobject.get("keyCode").getAsInt());
+                }
+            } catch (Throwable throwable) {
+            }
+        }
+    }
+
+    //add code
+    private static String f(Value<?> value) {
+        String s = value.wq() != null
+            ? (value.wq() instanceof Module ? ((Module)value.wq()).getModuleInfo().aliases()[0] + " Module" : ((Mode)value.wq()).getName() + " Mode")
+            : "Unknown";
+        return value.getName() + " in " + s;
+    }
+
+    //add code
+    private static void a(Value<?> value, JsonObject json) {
+        if (value instanceof ModeValue modevalue) {
+            if (json.has("value")) {
+                modevalue.co(json.get("value").getAsString());
+            }
+        } else if (value instanceof BooleanValue booleanvalue) {
+            if (json.has("value")) {
+                booleanvalue.setValue(json.get("value").getAsBoolean());
+            }
+        } else if (value instanceof NumberValue numbervalue) {
+            if (json.has("value")) {
+                numbervalue.n(json.get("value").getAsDouble());
+            }
+        } else if (value instanceof StringValue stringvalue) {
+            if (json.has("value")) {
+                stringvalue.n(json.get("value").getAsString().replaceAll("<percentsign>", "%"));
+            }
+        } else if (value instanceof BoundsNumberValue boundsnumbervalue) {
+            if (json.has("first")) {
+                boundsnumbervalue.n(json.get("first").getAsDouble());
+            }
+
+            if (json.has("second")) {
+                boundsnumbervalue.a(json.get("second").getAsDouble());
+            }
+        } else if (value instanceof ColorValue colorvalue) {
+            if (json.has("red")) {
+                Color color = colorvalue.wo();
+                colorvalue.n(new Color(json.get("red").getAsInt(), color.getGreen(), color.getBlue(), color.getAlpha()));
+            }
+
+            if (json.has("green")) {
+                Color color1 = colorvalue.wo();
+                colorvalue.n(new Color(color1.getRed(), json.get("green").getAsInt(), color1.getBlue(), color1.getAlpha()));
+            }
+
+            if (json.has("blue")) {
+                Color color2 = colorvalue.wo();
+                colorvalue.n(new Color(color2.getRed(), color2.getGreen(), json.get("blue").getAsInt(), color2.getAlpha()));
+            }
+
+            if (json.has("alpha")) {
+                Color color3 = colorvalue.wo();
+                colorvalue.n(new Color(color3.getRed(), color3.getGreen(), color3.getBlue(), json.get("alpha").getAsInt()));
+            }
+        } else if (value instanceof DragValue dragvalue) {
+            if (json.has("positionX")) {
+                double d0 = json.get("positionX").getAsDouble();
+                dragvalue.h(new Vector2d(d0, dragvalue.apP.y));
+                dragvalue.i(new Vector2d(d0, dragvalue.atg.y));
+            }
+
+            if (json.has("positionY")) {
+                double d1 = json.get("positionY").getAsDouble();
+                dragvalue.h(new Vector2d(dragvalue.apP.x, d1));
+                dragvalue.i(new Vector2d(dragvalue.atg.x, d1));
+            }
+
+            if (json.has("scaleX")) {
+                dragvalue.n(new Vector2d(json.get("scaleX").getAsDouble(), dragvalue.aHe.y));
+            }
+
+            if (json.has("scaleY")) {
+                dragvalue.n(new Vector2d(dragvalue.aHe.x, json.get("scaleY").getAsDouble()));
+            }
+        } else if (value instanceof ListValue listvalue) {
+            if (json.has("value")) {
+                String s = json.get("value").getAsString();
+
+                for (Object object : listvalue.getModes()) {
+                    if (object != null && object.toString().equalsIgnoreCase(s)) {
+                        listvalue.setValueAsObject(object);
+                    }
+                }
+            }
+        }
     }
 
     public static JsonObject d(java.io.File var0) {
